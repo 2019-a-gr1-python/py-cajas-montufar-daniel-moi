1195ea2765372fa1a450f4887fca322f69db06b9
Daniel1693 <dan.cajitas.16@gmail.com>
2019-05-22 08:58:17 -0500

    Manejo de Data frames

--- 03_Pandas/C_lectura_csv.py
+++ 03_Pandas/C_lectura_csv.py
@@ -5,3 +5,61 @@ Created on Wed May 22 07:52:07 2019
 @author: pasante_dgip3
 """
 
+import numpy as p
+import pandas as pd
+import os
+
+#Archivos texto -> JSON, CSV, HTML, XML, ...
+#Binary Files ->(asdasdasdasdasdasdasd)
+#Relational DataBases
+path = 'C:/Users/pasante_dgip3/Documents/GitHub/py-cajas-montufar-daniel-moi/03_Pandas/data/artwork_data.csv'
+df = pd.read_csv(
+        path,
+        nrows=5,
+        usecols=['id','artist'], #Para leer estas columnas 
+        index_col ='id'
+        
+        )
+
+columnas_a_usar = ['id','artist','title',
+                   'medium','year','acquisitionYear',
+                   'height','width','units']
+
+df_completo = pd.read_csv(
+        path,
+        usecols=columnas_a_usar,
+        index_col='id')
+
+path_guardado = 'C:/Users/pasante_dgip3/Documents/GitHub/py-cajas-montufar-daniel-moi/03_Pandas/data/artwork_data.pickle'
+
+df_completo.to_pickle(path_guardado)
+df_completo_pickle = pd.read_pickle(path_guardado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03_Pandas/D_lectura_json.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 08:32:25 2019
+
+@author: pasante_dgip3
+"""
+
