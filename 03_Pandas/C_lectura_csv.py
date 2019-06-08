8ce5386c649c336349046e1a3644fe8d57972c33
DanielCajas1693 <dan.cajitas.16@gmail.com>
2019-06-08 10:54:22 -0500

    Spyder

--- 03_Pandas/C_lectura_csv.py
+++ 03_Pandas/C_lectura_csv.py
@@ -1,24 +1,26 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 22 07:52:07 2019
+Created on Wed May 22 07:53:23 2019
 
-@author: pasante_dgip3
+@author: usrdel
 """
 
-import numpy as p
 import pandas as pd
 import os
 
-#Archivos texto -> JSON, CSV, HTML, XML, ...
-#Binary Files ->(asdasdasdasdasdasdasd)
-#Relational DataBases
-path = 'C:/Users/pasante_dgip3/Documents/GitHub/py-cajas-montufar-daniel-moi/03_Pandas/data/artwork_data.csv'
+# Archivos texto -> JSON, CSV, HTML, XML....
+# Binary Files -> (asdasdadasdasdasd)
+# Relational Databases
+
+
+path = '/Users/usrdel/Documents/GitHub/py-eguez-sarzosa-vicente-adrian/03_Pandas/data/csv/artwork_data.csv'
+
 df = pd.read_csv(
         path,
-        nrows=5,
-        usecols=['id','artist'], #Para leer estas columnas 
-        index_col ='id'
-        
+        nrows=100,
+        usecols=['id','artist'],
+        index_col='id'
         )
 
 columnas_a_usar = ['id','artist','title',
@@ -28,12 +30,18 @@ columnas_a_usar = ['id','artist','title',
 df_completo = pd.read_csv(
         path,
         usecols=columnas_a_usar,
-        index_col='id')
+        index_col='id'
+        )
 
-path_guardado = 'C:/Users/pasante_dgip3/Documents/GitHub/py-cajas-montufar-daniel-moi/03_Pandas/data/artwork_data.pickle'
+
+path_guardado = '/Users/usrdel/Documents/GitHub/py-eguez-sarzosa-vicente-adrian/03_Pandas/data/csv/artwork_data.pickle'
 
 df_completo.to_pickle(path_guardado)
+
 df_completo_pickle = pd.read_pickle(path_guardado)
+
+df_completo.shape
+
 
 
 
--- 03_Pandas/D_lectura_json.py
+++ 03_Pandas/D_lectura_json.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 22 08:32:25 2019
+Created on Wed May 22 08:33:45 2019
 
-@author: pasante_dgip3
+@author: usrdel
 """
-
 import json
 import pandas as pd
 import os
 
-path = 'C:/Users/pasante_dgip3/Documents/GitHub/py-cajas-montufar-daniel-moi/03_Pandas/data/artwork'
+path = 'C:/Users/danca/Downloads/py-cajas-montufar-daniel-moi-master/03_Pandas/data/artwork'
+
 
 archivo = '/a/000/a00001-1035.json'
 
@@ -19,31 +20,85 @@ llaves = ['id','all_artists',
           'title','medium',
           'dateText','acquisitionYear',
           'height','width','units']
-registro = []
-with open(path_archivo) as texto_json: 
-    contenido_json = json.load(texto_json)
-    print(type(contenido_json))
-    print(contenido_json)
-    registro_df_lista =[]
-    for llave in llaves:
-        valor = contenido_json[llave]
-        registro_df_lista.append(valor)
-    registro_df_tupla =tuple(registro_df_lista)
-    
-print (serie)
-df_chiquito = pd.DataFrame([registro_df_lista])
-df_chiquito_t = pd.DataFrame([registro_df_tupla])
 
-def leer_json(path,llaves):
-   with open(path_archivo) as texto_json: 
-       contenido_json = json.load(texto_json)
-    registro_df_lista =[]
+
+def leer_json(path_archivo,llaves):
+    with open(path_archivo) as texto_json:
+        contenido_json = json.load(texto_json)
+    registro_df_lista = []
     for llave in llaves:
         valor = contenido_json[llave]
         registro_df_lista.append(valor)
     return registro_df_lista
 
 leer_json(path_archivo, llaves)
+
+def leer_json_en_carpetas(directorio, llaves):
+    trabajos_arte = []
+    print(type(os.walk(directorio)))  # 
+    for path_raiz, lista_directorios, archivos in os.walk(directorio):
+        print(path_raiz)  
+        print(type(path_raiz))  # String -> Path Actual
+        print(lista_directorios)
+        print(type(lista_directorios))  #  List String directorios 
+        print(archivos)
+        print(type(archivos))  #  List Strings nombre archivos
+        
+        for nombre_archivo in archivos:
+            print(nombre_archivo)
+            if nombre_archivo.endswith('json'):
+                directorio_archivo = os.path.join(
+                        path_raiz,
+                        nombre_archivo
+                        )
+                pieza_arte = leer_json(directorio_archivo, llaves)
+                trabajos_arte.append(pieza_arte)
+                
+    df = pd.DataFrame.from_records(
+            trabajos_arte,
+            columns = llaves,
+            index = 'id'
+            )
+    return df
+
+
+df_artworks = leer_json_en_carpetas(path,llaves)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
